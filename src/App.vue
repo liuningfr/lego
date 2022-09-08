e5137a06b71a9f57b5f9a4dd13cd53693ef34491
liuning <liuning@kuaishou.com>
2022-09-08 11:02:38 +0800

    vue router

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
 <template>
-  <div class="homepage-container">
-    <a-layout :style="{background: '#fff'}">
-      <a-layout-header class="header">
-        <div class="page-title">
-          <router-link to="/">慕课乐高</router-link>
-        </div>
-      </a-layout-header>
-      <a-layout-content class="home-layout">
-        <router-view />
-      </a-layout-content>
-      <a-layout-footer>
-        © 慕课网（imooc.com）版权所有 | 津ICP备20000929号-2
-      </a-layout-footer>
-    </a-layout>
+  <div class="app-container">
+    <router-view />
   </div>
 </template>
 
