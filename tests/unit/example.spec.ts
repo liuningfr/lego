941ec6f70a67d9aa201f7982dd5aaed2ef43db33
liuning <liuning@kuaishou.com>
2022-10-01 18:20:41 +0800

    add event test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,5 +1,6 @@
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
+import Hello from '@/components/Hello.vue'
 import TemplateList from '@/components/TemplateList.vue'
 
 describe('HelloWorld.vue', () => {
@@ -8,7 +9,32 @@ describe('HelloWorld.vue', () => {
     const wrapper = shallowMount(HelloWorld, {
       props: { msg }
     })
-    // expect(wrapper.text()).toMatch(msg)
-    console.log(wrapper.findComponent(TemplateList))
+    expect(wrapper.get('h1').text()).toBe(msg)
+    expect(wrapper.findComponent(Hello).exists()).toBeTruthy()
+  })
+  it('should update the count when clicking the button', async () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(HelloWorld, {
+      props: { msg }
+    })
+    await wrapper.get('button').trigger('click')
+    expect(wrapper.get('button').text()).toBe('2')
+  })
+  it('should add todo when fill the input and click the add button', async () => {
+    const msg = 'new message'
+    const todoContent = 'buy milk'
+    const wrapper = shallowMount(HelloWorld, {
+      props: { msg }
+    })
+    await wrapper.get('input').setValue(todoContent)
+    expect(wrapper.get('input').element.value).toBe(todoContent)
+    await wrapper.get('.addTodo').trigger('click')
+    expect(wrapper.findAll('li')).toHaveLength(1)
+    expect(wrapper.get('li').text()).toBe(todoContent)
+    expect(wrapper.emitted()).toHaveProperty('send')
+    const events = wrapper.emitted('send')
+    if (events && events.length > 0) {
+      expect(events[0]).toEqual([todoContent])
+    }
   })
 })
