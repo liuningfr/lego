5f173d948a5b6ac8c51c6dce5365cc194f9183bd
liuning <liuning@kuaishou.com>
2022-10-01 23:04:46 +0800

    add UserProfile test

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  // transform: {
+  //   '^.+\\.vue$': 'vue-jest'
+  // },
+  // transformIgnorePatterns: [
+  //   '/!node_modules\\/lodash-es/'
+  // ]
 }
