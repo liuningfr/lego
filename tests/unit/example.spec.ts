4d678622b5579bd677330a304462abd9d30d0a84
liuning <liuning@kuaishou.com>
2022-10-01 20:35:06 +0800

    add beforeAll

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,51 +1,42 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, mount, VueWrapper } from '@vue/test-utils'
+import axios from 'axios'
+import flushPromises from 'flush-promises'
 import HelloWorld from '@/components/HelloWorld.vue'
 import Hello from '@/components/Hello.vue'
 import TemplateList from '@/components/TemplateList.vue'
-import axios from 'axios'
-import flushPromises from 'flush-promises'
-
 jest.mock('axios')
 const mockAxios = axios as jest.Mocked<typeof axios>
+const msg = 'new message'
+let wrapper: VueWrapper<any>
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+  beforeAll(() => {
+    wrapper = shallowMount(HelloWorld, {
       props: { msg }
     })
+  })
+  it('renders props.msg when passed', () => {
     expect(wrapper.get('h1').text()).toBe(msg)
     expect(wrapper.findComponent(Hello).exists()).toBeTruthy()
   })
   it('should update the count when clicking the button', async () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
     await wrapper.get('button').trigger('click')
     expect(wrapper.get('button').text()).toBe('2')
   })
   it('should add todo when fill the input and click the add button', async () => {
-    const msg = 'new message'
     const todoContent = 'buy milk'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
     await wrapper.get('input').setValue(todoContent)
     expect(wrapper.get('input').element.value).toBe(todoContent)
     await wrapper.get('.addTodo').trigger('click')
     expect(wrapper.findAll('li')).toHaveLength(1)
     expect(wrapper.get('li').text()).toBe(todoContent)
+    console.log(wrapper.emitted())
     expect(wrapper.emitted()).toHaveProperty('send')
     const events = wrapper.emitted('send')
     if (events && events.length > 0) {
       expect(events[0]).toEqual([todoContent])
     }
   })
-  it.only('should load user message when click the load button', async () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
+  it('should load user message when click the load button', async () => {
     mockAxios.get.mockResolvedValueOnce({ data: { username: 'viking'}})
     await wrapper.get('.loadUser').trigger('click')
     expect(mockAxios.get).toHaveBeenCalled()
@@ -55,15 +46,15 @@ describe('HelloWorld.vue', () => {
     expect(wrapper.find('.loading').exists()).toBeFalsy()
     expect(wrapper.get('.userName').text()).toBe('viking')
   })
-  it.only('should load error when return promise reject', async () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
+  it('should load error when return promise reject', async () => {
     mockAxios.get.mockRejectedValueOnce('error')
     await wrapper.get('.loadUser').trigger('click')
+    expect(mockAxios.get).toHaveBeenCalledTimes(1)
     await flushPromises()
     expect(wrapper.find('.loading').exists()).toBe(false)
     expect(wrapper.find('.error').exists()).toBe(true)
+  })
+  afterEach(() => {
+    mockAxios.get.mockReset()
   })
 })
