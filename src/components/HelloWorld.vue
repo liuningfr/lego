c1263d38bd40aec298291fce62544fcf24574bf2
liuning <liuning@kuaishou.com>
2022-10-01 13:24:45 +0800

    add unit jest

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>{{msg}}</h1>
+  <button @click="count++">{{count}}</button>
+  <hello msg="1234"></hello>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import Hello from './Hello.vue'
+export default defineComponent({
+  name: 'HelloWorld',
+  components: {
+    Hello
+  },
+  props: {
+    msg: String
+  },
+  setup() {
+    const count = ref(1)
+    return {
+      count
+    }
+  }
+})
+</script>
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,5 +1,6 @@
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
+import TemplateList from '@/components/TemplateList.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
@@ -7,6 +8,7 @@ describe('HelloWorld.vue', () => {
     const wrapper = shallowMount(HelloWorld, {
       props: { msg }
     })
-    expect(wrapper.text()).toMatch(msg)
+    // expect(wrapper.text()).toMatch(msg)
+    console.log(wrapper.findComponent(TemplateList))
   })
 })
