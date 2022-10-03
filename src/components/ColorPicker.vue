1c195e3bc5a2e357e8260b10fe9c193379d83009
liuning <liuning@kuaishou.com>
2022-10-03 20:58:29 +0800

    add colorpick test

---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="lego-color-picker">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+
+})
+</script>
+
+<style>
+
+</style>
